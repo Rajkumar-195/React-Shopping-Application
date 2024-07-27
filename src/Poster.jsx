78999d4d9e0124080f5200c7147130fd1fcda41c
RAJKUmar-35 <rajkumaranbu192@gmail.com>
2024-07-27 17:43:21 +0530

    Code_Updated

---
+++ src/Poster.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Poster() {
+  return (
+    <>
+      <div className="container-fluid posterDiv p-0">
+        <img src="/Images/hero_banner.jpg" className="d-block w-100" alt="Poster-Img" />
+      </div>
+    </>
+  );
+}
+
+export default Poster;
